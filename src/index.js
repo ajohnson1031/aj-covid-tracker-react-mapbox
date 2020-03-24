b0316850d6e582adc70e6cfa942e3725b530fb13
ajohnson1031 <aaron.johnson1031@gmail.com>
2020-03-24 12:26:11 -0400

    added peer dependencies

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import App from "./App";
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import { reducer } from "./redux-reducers";
 import { Provider } from "react-redux";
 
