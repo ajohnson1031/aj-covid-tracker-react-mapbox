29a331381b3334bee494ba2b9df4a8017ffa2efe
ajohnson1031 <aaron.johnson1031@gmail.com>
2020-03-24 12:02:03 -0400

    removed unnecessary imports

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import App from "./App";
 import { createStore, applyMiddleware } from "redux";
 import { reducer } from "./redux-reducers";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
